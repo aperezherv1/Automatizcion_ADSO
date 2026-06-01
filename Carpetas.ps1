5c8bb6abba07f74a0752bea65f0010113564e37f
aperezherv1 <aperezherv1@educacion.navarra.es>
2026-06-01 13:47:43 +0200

    Add files via upload

---
+++ Carpetas.ps1
@@ -0,0 +1,31 @@
+﻿#Creación carpeta para usuario
+mkdir D:\Ejercicio\Usuario1
+
+#Creación carpeta para uso personal
+mkdir D:\Ejercicio\Usuario1\Personal
+mkdir D:\Ejercicio\Usuario1\Personal\Musica
+mkdir D:\Ejercicio\Usuario1\Personal\Videos
+mkdir D:\Ejercicio\Usuario1\Personal\Fotos
+
+#Creación carpeta para uso Documentos 
+mkdir D:\Ejercicio\Usuario1\Documentos
+
+#Creación carpeta para uso Descargas
+mkdir D:\Ejercicio\Usuario1\Descargas
+
+#Creación carpeta para compartir documentos con usuarios
+mkdir D:\Ejercicio\Usuario1\Compartido
+
+#Creación carpetas departamentos de la empresa
+mkdir D:\Ejercicio\Usuario1\DEPARTAMENTOS\EOLICA
+mkdir D:\Ejercicio\Usuario1\DEPARTAMENTOS\INDUSTRIAL
+mkdir D:\Ejercicio\Usuario1\DEPARTAMENTOS\CALIDAD
+mkdir D:\Ejercicio\Usuario1\DEPARTAMENTOS\TIC
+mkdir D:\Ejercicio\Usuario1\DEPARTAMENTOS\RRHH
+mkdir D:\Ejercicio\Usuario1\DEPARTAMENTOS\OPERACIONES
+mkdir D:\Ejercicio\Usuario1\DEPARTAMENTOS\INGENIERIA
+
+#Ejemplo para parte 2
+New-Item -Path "D:\Ejercicio\Usuario1\Documentos\ArchivoAsier.txt" -Value "Copia de Asier" -Force
+
+
